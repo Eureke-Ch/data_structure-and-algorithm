079903b5a0f5d61e902ffefd2e408dc7f977b365
1998CHNB <1739224217@qq.com>
2021-11-10 22:41:33 +0800

    update binary_search

--- binary_search.cpp
+++ binary_search.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 using namespace std;
 
+
+// 二分一共可以分成三种类型分别是，分别是求取满足要求的值，满足要求的最小值，满足要求的最大值：
+// 1.满足要求的值：
 int binary_search(vector<int> nums, int target){
     int l = 0, r = nums.size()-1;
     int mid;
-    while(l < r){
+    while(l <= r){
         mid = (l+r)/2;
         if(nums[mid] == target){
             return mid;
@@ -17,6 +20,41 @@ int binary_search(vector<int> nums, int target){
     }
     return -1;
 }
+
+// 2.满足要求的最小值：
+int binary_search(vector<int> nums){
+    int l = 0, r = nums.size()-1;
+    while(l < r){
+        int mid = (l+r)/2;
+        if(check(mid)){
+            r = mid;
+        }else{
+            l = mid + 1;
+        }
+    }
+    if(check(l)){return l}
+    else return -1;
+}
+
+// 3.满足要求的最大值：
+int binary_search(vector<int> nums){
+    int l = 0, r = nums.size()-1;
+    while(l < r){
+        int mid = (l+r)/2;
+        if(check(mid)){
+            l = mid;
+        }else{
+           r = mid - 1;
+        }
+    }
+    if(check(l)){return l}
+    else return -1;
+}
+
+
+
+
+
 
 int main(){
     vector<int> nums {1, 2, 3, 4, 6, 21, 32, 52, 54, 345};
